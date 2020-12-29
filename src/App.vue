e2eadcf6e1e6907bd75a35a9834eb29e86c8e2c2
Marc Doerflinger <marc@doerf.li>
2020-12-29 23:37:09 +0100

    show spinner (closes #14)

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,30 @@
       <router-view/>
       <Footer />
     </div>
+    <div class="absolute w-full h-full top-0 left-0 flex h-screen bg-gray-700 opacity-80 z-100" v-if="showSpinner()">
+      <div class="m-auto flex-col justify-center">
+        <div>
+          <i class="fas fa-circle-notch fa-6x text-teal-300 fa-spin"></i>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-import Vue from 'vue'
-import Header from '@/components/Header'
-import { Component } from 'vue-property-decorator'
-import Footer from '@/components/Footer'
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
 
 @Component({
   components: { Footer, Header }
 })
 export default class App extends Vue {
+
+  private showSpinner(): boolean {
+    return this.$store.state.cases.cases.length == 0;
+  }
+
 }
 </script>
 
