e4ebb9a191fdbcb6ea49ef3152e3c985020c0e9c
Marc Doerflinger <marc@doerf.li>
2020-12-30 21:05:05 +0100

    footer format

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="py-6">
-    <i class="fab fa-github"></i> <Ref uri="https://github.com/doerfli/covid-numbers/">doerfli/covid-numbers</Ref>
-    | <i class="fab fa-twitter"></i> <Ref uri="https://twitter.com/doerfli">@doerfli</Ref>
-    | <i class="fas fa-database"></i> <Ref uri="https://github.com/openZH/covid_19">OpenZH/Covid19</Ref>
+  <div class="py-6 flex">
+    <div class="entry">
+      <i class="fab fa-github"></i> <Ref uri="https://github.com/doerfli/covid-numbers/">doerfli/covid-numbers</Ref>
+    </div>
+    <div class="entry">
+      <i class="fab fa-twitter"></i> <Ref uri="https://twitter.com/doerfli">@doerfli</Ref>
+    </div>
+    <div class="entrylast">
+      <i class="fas fa-database"></i> <Ref uri="https://github.com/openZH/covid_19">OpenZH/Covid19</Ref>
+    </div>
   </div>
 </template>
 
@@ -19,6 +25,14 @@ export default class Footer extends Vue {
 </script>
 
 <style scoped>
-
+  .entry {
+    @apply px-3;
+    @apply border-solid;
+    @apply border-r-2;
+    @apply border-gray-200;
+  }
+  .entrylast {
+    @apply px-3;
+  }
 </style>
 
