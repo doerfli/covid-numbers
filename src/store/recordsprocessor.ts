343e6f1dab14e7f68324612844e811e72fb0d46d
Marc Doerflinger <marc@doerf.li>
2021-01-10 14:12:03 +0100

    restructure (#22)

--- src/store/recordsprocessor.ts
+++ src/store/recordsprocessor.ts
@@ -31,10 +31,97 @@ export  default class RecordsProcessor {
     "ZH"
   ]
 
+  // eslint-disable-next-line
+  public process(records: any[]): Map<string, DailyDataSet[]> {
+    // canton -> Array<DailyData>
+    const dataMap: Map<string, DailyDataSet[]> = this.initializedMap();
+    // console.log(payload.records);
+    const totalCh = new Array<DailyDataSet>();
+    let currentDay: string = records[0].date;
+
+    // loop over all records and store in DailyData structure by canton
+    // eslint-disable-next-line
+    records.forEach((val: any) => {
+      // console.log(val);
+
+      const canton = val.abbreviation_canton_and_fl;
+      if (canton === "FL") {
+        return;
+      }
+      let record = RecordsProcessor.parseRecord(val);
+
+      if (record.confCases == -1) {
+        const latest = dataMap.get(canton)?.slice(-1)[0] ?? {
+          confCases: 0,
+          currHosp: 0,
+          currIcu: 0,
+          deceased: 0,
+        } as DailyDataSet;
+        record = {
+          date: val.date,
+          confCases: latest.confCases,
+          currHosp: latest.currHosp,
+          currIcu: latest.currIcu,
+          deceased: latest.deceased,
+        } as DailyDataSet;
+      }
+
+      currentDay = this.updateCurrentDayData(currentDay, record.date, totalCh, dataMap);
+
+      const cantonData = dataMap.get(canton);
+      if (dataMap.has(canton) && cantonData !== undefined) {
+        cantonData.push(record);
+      } else {
+        console.log("undefined dataMap entry for " + canton);
+      }
+    });
+
+    currentDay = this.updateCurrentDayData(currentDay, "2100-01-01", totalCh, dataMap);
+
+    // set total ch data
+    dataMap.set("CH", totalCh);
+
+    return dataMap;
+  }
+
   private initializedMap(): Map<string,Array<DailyDataSet>> {
     const map = new Map<string,Array<DailyDataSet>>();
     this.CANTONS.forEach((i) => map.set(i, new Array<DailyDataSet>()));
     return map;
+  }
+
+  // eslint-disable-next-line
+  private static parseRecord (val: any): DailyDataSet {
+    if (val.ncumul_conf == "") {
+      return {
+        date: val.date,
+        confCases: -1,
+        currHosp: -1,
+        currIcu: -1,
+        deceased: -1
+      } as DailyDataSet;
+    }
+    return {
+      date: val.date,
+      confCases: parseInt(val.ncumul_conf) || 0,
+      currHosp: parseInt(val.current_hosp) || 0,
+      currIcu: parseInt(val.current_icu) || 0,
+      deceased: parseInt(val.ncumul_deceased) || 0
+    } as DailyDataSet;
+  }
+
+  /**
+   * adds missing entries and calculates totals for day across all cantons
+   */
+  private updateCurrentDayData (currentDay: string, recordDate: string, totalCh: DailyDataSet[], dataMap: Map<string, DailyDataSet[]>) {
+    if (recordDate !== currentDay) {
+      // console.log(data.date + " <- " + currentDay)
+      this.completeDataMap(currentDay, dataMap);
+      this.calculateTotal(currentDay, dataMap, totalCh);
+      return recordDate;
+    }
+
+    return currentDay;
   }
 
   /**
@@ -101,93 +188,6 @@ export  default class RecordsProcessor {
     } as DailyDataSet;
     // console.log(d);
     totalCh.push(d);
-  }
-
-  /**
-   * adds missing entries and calculates totals for day across all cantons
-   */
-  private updateCurrentDayData (currentDay: string, recordDate: string, totalCh: DailyDataSet[], dataMap: Map<string, DailyDataSet[]>) {
-    if (recordDate !== currentDay) {
-      // console.log(data.date + " <- " + currentDay)
-      this.completeDataMap(currentDay, dataMap);
-      this.calculateTotal(currentDay, dataMap, totalCh);
-      return recordDate;
-    }
-
-    return currentDay;
-  }
-
-  // eslint-disable-next-line
-  private static parseRecord (val: any): DailyDataSet {
-    if (val.ncumul_conf == "") {
-      return {
-        date: val.date,
-        confCases: -1,
-        currHosp: -1,
-        currIcu: -1,
-        deceased: -1
-      } as DailyDataSet;
-    }
-    return {
-      date: val.date,
-      confCases: parseInt(val.ncumul_conf) || 0,
-      currHosp: parseInt(val.current_hosp) || 0,
-      currIcu: parseInt(val.current_icu) || 0,
-      deceased: parseInt(val.ncumul_deceased) || 0
-    } as DailyDataSet;
-  }
-
-  // eslint-disable-next-line
-  public process(records: any[]): Map<string, DailyDataSet[]> {
-    // canton -> Array<DailyData>
-    const dataMap: Map<string, DailyDataSet[]> = this.initializedMap();
-    // console.log(payload.records);
-    const totalCh = new Array<DailyDataSet>();
-    let currentDay: string = records[0].date;
-
-    // loop over all records and store in DailyData structure by canton
-    // eslint-disable-next-line
-    records.forEach((val: any) => {
-      // console.log(val);
-
-      const canton = val.abbreviation_canton_and_fl;
-      if (canton === "FL") {
-        return;
-      }
-      let record = RecordsProcessor.parseRecord(val);
-
-      if (record.confCases == -1) {
-        const latest = dataMap.get(canton)?.slice(-1)[0] ?? {
-          confCases: 0,
-          currHosp: 0,
-          currIcu: 0,
-          deceased: 0,
-        } as DailyDataSet;
-        record = {
-          date: val.date,
-          confCases: latest.confCases,
-          currHosp: latest.currHosp,
-          currIcu: latest.currIcu,
-          deceased: latest.deceased,
-        } as DailyDataSet;
-      }
-
-      currentDay = this.updateCurrentDayData(currentDay, record.date, totalCh, dataMap);
-
-      const cantonData = dataMap.get(canton);
-      if (dataMap.has(canton) && cantonData !== undefined) {
-        cantonData.push(record);
-      } else {
-        console.log("undefined dataMap entry for " + canton);
-      }
-    });
-
-    currentDay = this.updateCurrentDayData(currentDay, "2100-01-01", totalCh, dataMap);
-
-    // set total ch data
-    dataMap.set("CH", totalCh);
-
-    return dataMap;
   }
 
 }
