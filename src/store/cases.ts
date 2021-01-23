08a4e0a13cbeae0ac582101513acf454ab2c765f
Marc Doerflinger <marc@doerf.li>
2021-01-23 13:14:09 +0100

    refactoring (#51)

--- src/store/cases.ts
+++ src/store/cases.ts
@@ -6,7 +6,6 @@ import RecordsProcessor from '@/utils/records-processor'
 import DailyDataSet from '@/model/dailyDataSet'
 import DailyDiff from '@/model/dailyDiff'
 import DailyIncidence from '@/model/dailyIncidence'
-import getProperty from '@/utils/get-property'
 import StaticData from '@/store/staticdata'
 
 function calculateAverageValue (newCases: Array<DailyDiff>, averageWindowSize: number) {
@@ -81,44 +80,13 @@ const casesModule: Module<any, any> = {
       }
       return cantonData[0].data;
     }),
-    calculateDailyDiff: ((state, getters) =>
-          (canton: string,
-          // eslint-disable-next-line
-          fieldName: any,
-          averageWindowSize = 0): Array<DailyDiff> => {
-      let last = 0;
-      let newCases = getters.dataPerCanton(canton).map((dataPoint: DailyDataSet, idx: number) => {
-        const value = getProperty(dataPoint, fieldName);
-        if (idx == 0) { // first datapoint
-          last = value;
-          return null;
-        }
-        const n = value - last;
-        last = value;
-
-        return {
-          date: dataPoint.date,
-          fieldName: fieldName,
-          value: n
-        } as DailyDiff;
-      }).filter((v: DailyDiff | null) => v != null) as Array<DailyDiff>;
-
-      if (averageWindowSize > 0) {
-        // calculate sliding window average
-        newCases = calculateAverageValue(newCases, averageWindowSize)
-      }
-
-      // console.log(0);
-      // console.log(newCases);
-      return newCases;
-    }),
     incidence: ((state, getters) =>
       (canton: string,
         // eslint-disable-next-line
         fieldName: any = "confCases",
         windowSize = 7): Array<DailyDiff> => {
 
-      return getters.calculateDailyDiff(canton, fieldName, windowSize).map((dataPoint: DailyDiff, idx: number, arr: DailyDiff[]) => {
+      return getters.dataPerCanton(canton).map((dataPoint: DailyDataSet, idx: number, arr: Array<DailyDataSet>) => {
         let incidence = null;
 
         // calculate from first value data point up to second last day (current day data is rarely complete)
@@ -128,7 +96,7 @@ const casesModule: Module<any, any> = {
             pop = StaticData.getTotalPopulation();
           }
           const totalLastWindowSlice = arr.slice(idx - windowSize + 1, idx + 1)
-            .map((x) => x.value)
+            .map((x) => x.confCasesChg)
             .reduce((sum, current) => sum + current);
           incidence = totalLastWindowSlice / pop * 100000;
         }
