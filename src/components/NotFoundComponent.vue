932e7f8c8aeab84450eb27db8ab7782311e55031
Marc Doerflinger <marc@doerf.li>
2021-02-07 14:29:55 +0100

    add 404 component (#104)

---
+++ src/components/NotFoundComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="text-2xl 2xl:text-pink-600 font-bold py-4">
+    The requested page does not exist
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+    components: {}
+  })
+  export default class NotFoundComponent extends Vue {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
+
