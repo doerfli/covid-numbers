746235d4a74ef2262f50c7b191e7de84d49b0e38
Marc Doerflinger <marc@doerf.li>
2021-02-07 14:34:50 +0100

    add alternative links (closes #104)

--- src/components/NotFoundComponent.vue
+++ src/components/NotFoundComponent.vue
@@ -1,14 +1,21 @@
 <template>
-  <div class="text-2xl 2xl:text-pink-600 font-bold py-4">
-    The requested page does not exist
+  <div>
+    <div class="text-2xl 2xl:text-pink-600 font-bold py-4">
+      The requested page does not exist
+    </div>
+    <div>
+      Go to the <Ref uri="/">Homepage (/)</Ref><br/>
+      Go to the <Ref uri="/sitemap.xml">Sitemap (/sitemap.xml)</Ref>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import Ref from '@/components/base/Ref.vue'
 
 @Component({
-    components: {}
+    components: { Ref }
   })
   export default class NotFoundComponent extends Vue {
 
